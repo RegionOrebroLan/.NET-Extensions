7a26c8350fc7af3677a5348cd17c7214511c225e
Hans Kindberg <hans.kindberg@regionorebrolan.se>
2020-01-27 07:30:46 +0100

    String-extension: Like

--- Source/Project/Extensions/StringExtension.cs
+++ Source/Project/Extensions/StringExtension.cs
@@ -1,8 +1,52 @@
-﻿namespace RegionOrebroLan.Extensions
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace RegionOrebroLan.Extensions
 {
 	public static class StringExtension
 	{
+		#region Fields
+
+		public const char DefaultWildcardCharacter = '*';
+
+		#endregion
+
 		#region Methods
+
+		public static bool Like(this string value, string pattern)
+		{
+			return value.Like(pattern, DefaultWildcardCharacter);
+		}
+
+		public static bool Like(this string value, string pattern, char wildcardCharacter)
+		{
+			return value.Like(pattern, wildcardCharacter, true);
+		}
+
+		public static bool Like(this string value, string pattern, bool caseInsensitive)
+		{
+			return value.Like(pattern, DefaultWildcardCharacter, caseInsensitive);
+		}
+
+		public static bool Like(this string value, string pattern, char wildcardCharacter, bool caseInsensitive)
+		{
+			if(value == null)
+				throw new ArgumentNullException(nameof(value));
+
+			if(pattern == null)
+				throw new ArgumentNullException(nameof(pattern));
+
+			var regexOptions = RegexOptions.Compiled;
+
+			if(caseInsensitive)
+				regexOptions |= RegexOptions.IgnoreCase;
+
+			var regexPattern = pattern.Replace(wildcardCharacter.ToString(CultureInfo.InvariantCulture), "*");
+			regexPattern = "^" + Regex.Escape(regexPattern).Replace("\\*", ".*") + "$";
+
+			return Regex.IsMatch(value, regexPattern, regexOptions);
+		}
 
 		public static string RemoveIfFirst(this string value, char characterToRemove)
 		{
---
+++ Source/Unit-tests/Extensions/StringExtensionTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegionOrebroLan.Extensions;
+
+namespace RegionOrebroLan.UnitTests.Extensions
+{
+	[TestClass]
+	public class StringExtensionTest
+	{
+		#region Methods
+
+		[TestMethod]
+		public void Like_Test()
+		{
+			Assert.IsTrue("Test".Like("test"));
+			Assert.IsFalse("Test".Like("test", false));
+
+			Assert.IsFalse("My Test Text".Like("test"));
+			Assert.IsTrue("My Test Text".Like("*test*"));
+			Assert.IsFalse("My Test Text".Like("*test*", false));
+		}
+
+		#endregion
+	}
+}
