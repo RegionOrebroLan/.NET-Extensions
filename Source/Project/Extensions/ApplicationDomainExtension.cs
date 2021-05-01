7f1462ea389c44553846fcf516e1f000af6680d0
HansKindberg <hans.kindberg@gmail.com>
2021-05-01 12:09:52 +0200

    Refactoring

--- Source/Project/Extensions/ApplicationDomainExtension.cs
+++ Source/Project/Extensions/ApplicationDomainExtension.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+using System;
 using System.Globalization;
 
 namespace RegionOrebroLan.Extensions
@@ -8,17 +7,15 @@ namespace RegionOrebroLan.Extensions
 	{
 		#region Methods
 
-		[SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "DataDirectory")]
 		public static string GetDataDirectoryPath(this IApplicationDomain applicationDomain)
 		{
 			if(applicationDomain == null)
 				throw new ArgumentNullException(nameof(applicationDomain));
 
-			const string dataDirectory = "DataDirectory";
-			var dataDirectoryPath = (string) applicationDomain.GetData(dataDirectory);
+			var dataDirectoryPath = (string) applicationDomain.GetData(AppDomainExtension.DataDirectoryName);
 
 			if(dataDirectoryPath == null)
-				throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "The variable \"{0}\" is not set for the application-domain.", dataDirectory));
+				throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "The variable \"{0}\" is not set for the application-domain.", AppDomainExtension.DataDirectoryName));
 
 			return dataDirectoryPath;
 		}
